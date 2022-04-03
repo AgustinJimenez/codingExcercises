83c7b229dc64e80e2ca6770dfb893ad74276ee00
Agustin <agus.jimenez.caba@gmail.com>
2022-04-03 17:24:11 -0400

    no comments

--- test/a_company_performing_an_analysis_on_the_computers.test.ts
+++ test/a_company_performing_an_analysis_on_the_computers.test.ts
@@ -21,18 +21,26 @@ import { assert } from "chai";
 
 const test = (x: number, space: number[]) => {
   const mins = [];
-
-  for (let c = 0; c < space.length / x; c++)
-    mins.push(Math.min(...space.slice(c, c + x)));
+  let arr = [];
+  for (let c = 0; c < space.length; c++) {
+    const slice = space.slice(c, c + x);
+    if (slice.length !== x) break;
+    arr[c] = slice;
+    const slice_min = Math.min(...slice);
+    mins.push(slice_min);
+  }
 
   const max = Math.max(...mins);
 
-  // console.log({x, space,arr, mins, max})
+  console.log({ x, space, arr, mins, max });
   return max;
 };
 
 describe("solution", function () {
   it("should handle basic tests", function () {
     assert.strictEqual(test(4, [62, 64, 77, 75, 71, 60, 79, 75]), 64);
+    assert.strictEqual(test(2, [8, 2, 4, 6]), 4);
+    assert.strictEqual(test(1, [1, 2, 3, 1, 2]), 3);
+    assert.strictEqual(test(2, [1, 1, 1]), 1);
   });
 });
